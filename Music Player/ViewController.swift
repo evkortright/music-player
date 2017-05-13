c3b9a1f8c97e67132311423c74ac696a00ad0abd
Enrique V. Kortright <enrique.kortright@us.ibm.com>
2017-05-13 14:29:21 -0500

    Complete first version.

--- Music Player/ViewController.swift
+++ Music Player/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var positionSlider: UISlider!
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let audioPath = Bundle.main.path(forResource: "Dance Macabre", ofType: "mp3")
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            player.volume = 0.5
+            titleLabel.text = "Dance Macabre"
+            positionSlider.value = Float(player.currentTime)
+            volumeSlider.value = player.volume
+            
+            Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updatePosition), userInfo: nil, repeats: true)
+        } catch {
+            print("Error loading mp3.")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updatePosition() {
+        print(player.currentTime)
+        let position = Float(player.currentTime) / Float(player.duration)
+        positionSlider.value = position
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        player.play()
     }
 
-
+    @IBAction func pauseTapped(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBAction func stopTapped(_ sender: Any) {
+        player.currentTime = TimeInterval(0.0)
+        positionSlider.value = 0.0
+        player.stop()
+    }
+    
+    @IBAction func positionSliderChanged(_ sender: Any) {
+        print("positionSlider: \(positionSlider.value)")
+        let position = Float(player.duration) * positionSlider.value
+        player.currentTime = TimeInterval(position)
+    }
+    
+    @IBAction func volumeSliderChanged(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
 }
 
